d3bd1a2397538d16d221d7b0473fbd666485c377
alvesph <alvesph1@gmail.com>
2024-10-07 17:25:33 -0300

    chore: cleaning unnecessary values and changing registry port

--- toolkit/clean_docker.sh
+++ toolkit/clean_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker system prune -f
-docker images --format '{{.Repository}}:{{.Tag}}' | grep 'localhost:5000/k3d-registry.localhost_5000_' | xargs -r docker rmi -f
+docker images --format '{{.Repository}}:{{.Tag}}' | grep 'localhost:5006/k3d-registry.localhost_5006_' | xargs -r docker rmi -f
